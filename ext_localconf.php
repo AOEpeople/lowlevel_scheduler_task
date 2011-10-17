26fdae537e4d437aad56a3a9446a0e02858736e6
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-17 12:47:56 +0000

    Add support for "tx_cachecleaner"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/lowlevel_scheduler_task/trunk@53058 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,3 +19,13 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_lowlevelsche
 	'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:lowlevelconnect_task_syslog.description',
 	'additionalFields' => 'tx_lowlevelschedulertask_task_syslog_AdditionalFieldProvider'
 );
+
+
+if (t3lib_extMgm::isLoaded('cachecleaner')) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_lowlevelschedulertask_task_cachecleaner] = array(
+		'extension'        => $_EXTKEY,
+		'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:lowlevelconnect_task_cachecleaner.name',
+		'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:lowlevelconnect_task_cachecleaner.description',
+		'additionalFields' => 'tx_lowlevelschedulertask_task_cachecleaner_AdditionalFieldProvider'
+	);
+}
