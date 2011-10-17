1d1a8752f5a23ac8fdbe1fe106b117656ab25bdc
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-17 13:07:21 +0000

    add label & autoload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/lowlevel_scheduler_task/trunk@53060 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -6,6 +6,8 @@ $extensionPath = t3lib_extMgm::extPath('lowlevel_scheduler_task');
 return array(
 	'tx_lowlevelschedulertask_task_cleanup' => $extensionPath . 'task/class.tx_lowlevelschedulertask_task_cleanup.php',
 	'tx_lowlevelschedulertask_task_cleanup_additionalfieldprovider' => $extensionPath . 'task/class.tx_lowlevelschedulertask_task_cleanup_AdditionalFieldProvider.php',
+	'tx_lowlevelschedulertask_task_cachecleaner' => $extensionPath . 'task/class.tx_lowlevelschedulertask_task_cachecleaner.php',
+	'tx_lowlevelschedulertask_task_cachecleaner_additionalfieldprovider' => $extensionPath . 'task/class.tx_lowlevelschedulertask_task_cachecleaner_AdditionalFieldProvider.php',
 	'tx_lowlevelschedulertask_task_syslog' => $extensionPath . 'task/class.tx_lowlevelschedulertask_task_syslog.php',
 	'tx_lowlevelschedulertask_task_syslog_additionalfieldprovider' => $extensionPath . 'task/class.tx_lowlevelschedulertask_task_syslog_AdditionalFieldProvider.php',
 	'tx_lowlevelschedulertask_task_updaterefindex' => $extensionPath . 'task/class.tx_lowlevelschedulertask_task_updateRefindex.php',
